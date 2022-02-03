c0d9e18202ff20c7b41dfa45030e62d144812501
ggOS <89165447+getggos@users.noreply.github.com>
2022-02-02 19:09:44 -0500

    v0.9.0.1

--- VPN/vpn-disable.default.bat
+++ VPN/vpn-disable.default.bat
@@ -7,6 +7,13 @@ setlocal ENABLEDELAYEDEXPANSION
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || ( echo This script must be Run as Administrator. && pause && exit /b 1 )
 
+:: DISABLE VPN DRIVERS
+sc config PptpMiniport start=disabled >nul 2>&1
+sc config RasAgileVpn start=disabled >nul 2>&1
+sc config Rasl2tp start=disabled >nul 2>&1
+sc config RasSstp start=disabled >nul 2>&1
+sc config RasPppoe start=disabled >nul 2>&1
+
 :: DISABLE VPN SERVICES
 sc config RasMan start=disabled >nul 2>&1
 
--- VPN/vpn-enable.bat
+++ VPN/vpn-enable.bat
@@ -7,6 +7,13 @@ setlocal ENABLEDELAYEDEXPANSION
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || ( echo This script must be Run as Administrator. && pause && exit /b 1 )
 
+:: ENABLE VPN DRIVERS
+sc config PptpMiniport start=demand >nul 2>&1
+sc config RasAgileVpn start=demand >nul 2>&1
+sc config Rasl2tp start=demand >nul 2>&1
+sc config RasSstp start=demand >nul 2>&1
+sc config RasPppoe start=demand >nul 2>&1
+
 :: ENABLE VPN SERVICES
 sc config RasMan start=auto >nul 2>&1
 
