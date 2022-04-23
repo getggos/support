91bc6dbf2dac421d3367309b6f44167d58409f2c
ggOS <89165447+getggos@users.noreply.github.com>
2022-04-23 14:57:49 -0400

    v0.9.3

--- Lanman Workstation/lanman-workstation-disable.default.bat
+++ Lanman Workstation/lanman-workstation-disable.default.bat
@@ -12,6 +12,10 @@ sc config rdbss start=demand >nul 2>&1
 sc config KSecPkg start=disabled >nul 2>&1
 sc config LanmanWorkstation start=disabled >nul 2>&1
 
+:: DISABLE AUTOSHARE
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer\Parameters" /v "AutoShareServer" /t REG_DWORD /d "0" /f >nul 2>&1
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer\Parameters" /v "AutoShareWks" /t REG_DWORD /d "0" /f >nul 2>&1
+
 echo Lanman Workstation has been disabled. Please restart your computer.
 pause
 
--- Lanman Workstation/lanman-workstation-enable.bat
+++ Lanman Workstation/lanman-workstation-enable.bat
@@ -12,6 +12,10 @@ sc config rdbss start=system >nul 2>&1
 sc config KSecPkg start=boot >nul 2>&1
 sc config LanmanWorkstation start=auto >nul 2>&1
 
+:: ENABLE AUTOSHARE
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer\Parameters" /v "AutoShareServer" /t REG_DWORD /d "1" /f >nul 2>&1
+reg add "HKLM\System\CurrentControlSet\Services\LanmanServer\Parameters" /v "AutoShareWks" /t REG_DWORD /d "1" /f >nul 2>&1
+
 echo Lanman Workstation has been enabled. Please restart your computer.
 pause
 
