5786a6d00c3ab0746b0e2be9afa5dd9b5c5fb222
Vojtass <101590573+Vojtazz@users.noreply.github.com>
2022-06-02 17:39:51 +0200

    Troubleshooting script fix

--- Troubleshooting/windows-troubleshooting-enable.bat
+++ Troubleshooting/windows-troubleshooting-enable.bat
@@ -7,7 +7,7 @@ setlocal ENABLEDELAYEDEXPANSION
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || ( echo This script must be Run as Administrator. && pause && exit /b 1 )
 
-:: DISABLE DIAGNOSTICS & TROUBLESHOOTING SERVICES
+:: ENABLE DIAGNOSTICS & TROUBLESHOOTING SERVICES
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Windows" /v "NoInteractiveServices" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\DiagLog" /v "Start" /t REG_DWORD /d "1" /f >nul 2>&1
 sc config DPS start=auto >nul 2>&1
@@ -15,4 +15,4 @@ sc config DPS start=auto >nul 2>&1
 echo Diagnostics and Troubleshooting services have been enabled. Please restart your computer.
 pause
 
-/exit /b 0
+exit /b 0
